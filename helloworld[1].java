344b92557590dd6a9127543ea916f54d46182b83
vikrambhabutmal <34740728+vikrambhabutmal@users.noreply.github.com>
2017-12-21 19:25:12 +0530

    Delete helloworld[1].java

--- helloworld[1].java
+++
@@ -1,10 +0,0 @@
-class Programming {
-  //constructor method
-  Programming() {
-    System.out.println("Constructor method called.");
-  }
-}public class helloworld{
-  public static void main(String[] args) {
-    Programming object = new Programming(); //creating object
-  }
-}
